388170b4789f8183e315431729986541f7caeade
yana.gerasimchuk-vrp <yana.gerasimchuk-vrp@bullhorn.com>
2019-06-04 21:23:46 +0300

    Removed dependency

--- index.js
+++ index.js
@@ -16,3 +16,54 @@ yargs
     .help()
     .argv;
 
+
+
+/*
+const Promise = require('bluebird');
+const { MongoClient } = require('mongodb');
+
+Promise.promisifyAll(MongoClient);
+
+MongoClient.connect('mongodb://admin:YanaGeras12061996@ds149706.mlab.com:49706/paper-games-test', { useNewUrlParser: true }, (err, client) => {
+    const db = client.db('paper-games-test');
+    const collection = db.collection('test2');
+
+    let docs = [];
+    for (let i = 500000; i < 1000000; i++) {
+        docs.push({
+            name: "name",
+            firstName: 'firstName',
+            age: 18,
+            increment: i,
+            date: new Date(),
+            text: null,
+            text2: undefined
+        });
+    }
+    collection.insertMany(docs, { w: 1 });
+});*/
+
+/*
+const { getDriverInstance } = require('./lib/driver');
+const db = getDriverInstance({
+    driver: 'mongodb',
+    path: __dirname,
+    uri: 'mongodb://admin:YanaGeras12061996@ds217976.mlab.com:17976/paper-games',
+    database: 'paper-games',
+    //uri: 'mongodb://admin:YanaGeras12061996@ds217976.mlab.com:17976/paper-games',
+    //database: 'paper-games',
+    format: 'json'
+});
+
+db.doBackup();*/
+
+/*
+const { getDriverInstance } = require('./lib/driver');
+const db = getDriverInstance({
+    driver: 'mongodb',
+    uri: 'mongodb://admin:YanaGeras12061996@ds151994.mlab.com:51994/test-migrate',
+    database: 'test-migrate',
+    path: '/Users/yanagerasimchuk/self-education/projects/db-data-manupulate/1559658944182_test-backup/'
+});
+db.doRestore();*/
+
